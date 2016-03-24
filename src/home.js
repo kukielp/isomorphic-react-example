d4d4d8a306fc899bb2f349a34fc0bb1131a31f45
Paul <kukielp@gmail.com>
2016-03-25 10:39:33 +1100

    Add google analytics to demo

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
 		return (
 			<div>
 				<h1>Home Page</h1>
-				<p>Avalon is so not melb dandenong, old melbourne gaol east brunswick club chapel street kylie minogue chaddie, naked for satan etihad stadium kath and kim black is alway in fashion the rebels, the G flemington racecourse the espy the australian open carlton, cate blanchette geoffrey rush south melb dim sims.</p>
+				<p>Avalon is so not melb dandenong, old melbourne gaol east brunswick club chapel street kylie minogue chaddie, etihad stadium kath and kim black is alway in fashion the rebels, the G flemington racecourse the espy the australian open carlton, cate blanchette geoffrey rush south melb dim sims.</p>
 			</div>
 		);
 	}
